4c872158fe0a702ce9625b819ebf6e15e4d23f02
Tammam Kbeili <tammam.kbeili@gmail.com>
2016-04-29 10:40:53 -0700

    modify the info method message

--- car.rb
+++ car.rb
@@ -7,7 +7,7 @@ class Car
   end
 
   def info
-    "This car has #{color} color and a capacity of #{capacity}"
+    "This car has a #{@color} color and a capacity of #{@capacity}"
   end
 
   def stop
